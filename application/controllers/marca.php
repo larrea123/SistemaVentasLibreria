7149e5c0d284dc9f5850af088d3f09fddc90c152
lore <larrea.lorena.939@gmail.com>
2023-08-20 21:50:11 -0400

    Creacion de las consultas(modificar, eliminar y deshabilitar) en el modelo marca y creacion de las funciones en el controlador marca

--- application/controllers/marca.php
+++ application/controllers/marca.php
@@ -93,5 +93,127 @@ class Marca extends CI_Controller {
 	}
 
 
+    public function eliminarbd()
+    {
+        $idmarca=$_POST['idmarca'];
+        $this->marca_model->eliminarmarca($idmarca);
+        redirect('marca/index','refresh');
+    }
+
+    public function modificar()
+    {
+        if($this->session->userdata('rol')=='admin')
+        {
+            $idmarca=$_POST['idmarca'];
+            $data['infomarca']=$this->marca_model->recuperarmarca($idmarca);
+
+            $this->load->view('inc/cabecera');
+            $this->load->view('inc/menulateral');
+            $this->load->view('inc/menusuperior');
+            $this->load->view('marca/marca_modificar',$data);
+            $this->load->view('inc/creditos');	
+            $this->load->view('inc/pie');
+        }
+        else
+        {
+            if($this->session->userdata('rol')=='contador')
+            {
+                $idmarca=$_POST['idmarca'];
+                $data['infomarca']=$this->marca_model->recuperarmarca($idmarca);
+    
+                $this->load->view('inc/cabecera');
+                $this->load->view('inc/menulateral_contador');
+                $this->load->view('inc/menusuperior');
+                $this->load->view('marca/contador/marca_modificar',$data);
+                $this->load->view('inc/creditos');	
+                $this->load->view('inc/pie');
+            }
+            else
+            {
+                $idmarca=$_POST['idmarca'];
+                $data['infomarca']=$this->marca_model->recuperarmarca($idmarca);
+    
+                $this->load->view('inc/cabecera');
+                $this->load->view('inc/menulateral_vendedor');
+                $this->load->view('inc/menusuperior');
+                $this->load->view('marca/vendedor/marca_modificar',$data);
+                $this->load->view('inc/creditos');	
+                $this->load->view('inc/pie');
+            }
+        } 
+    }
+
+    public function modificarbd()
+    {
+        $idmarca=$_POST['idmarca'];
+
+        $data['idUsuario']=$this->session->userdata('idusuario');
+        $data['nombreMarca']=strtoupper($_POST['NombreMarca']);
+        $data['fechaActualizacion']=date('Y-m-d H:i:s');
+        
+        $this->marca_model->modificarmarca($idmarca,$data);
+        redirect('marca/index','refresh');
+    }
+    public function deshabilitarbd() 
+    {
+        $idmarca=$_POST['idmarca'];
+        $data['estado']='0';
+
+        $this->marca_model->modificarmarca($idmarca,$data);
+        redirect('marca/index','refresh');
+    }
+
+    public function deshabilitados()
+	{
+        if($this->session->userdata('rol')=='admin')
+        {
+            $lista=$this->marca_model->listamarcasdeshabilitados();
+            $data['marca']=$lista;
+
+            $this->load->view('inc/cabecera');
+            $this->load->view('inc/menulateral');
+            $this->load->view('inc/menusuperior');
+            $this->load->view('marca/marca_lista_deshabilitados',$data);
+            $this->load->view('inc/creditos');	
+            $this->load->view('inc/pie');
+        }
+        else
+        {
+            if($this->session->userdata('rol')=='contador')
+            {
+                $lista=$this->marca_model->listamarcasdeshabilitados();
+                $data['marca']=$lista;
+
+                $this->load->view('inc/cabecera');
+                $this->load->view('inc/menulateral_contador');
+                $this->load->view('inc/menusuperior');
+                $this->load->view('marca/contador/marca_lista_deshabilitados',$data);
+                $this->load->view('inc/creditos');	
+                $this->load->view('inc/pie');
+            }
+            else
+            {
+                
+                $lista=$this->marca_model->listamarcasdeshabilitados();
+                $data['marca']=$lista;
+
+                $this->load->view('inc/cabecera');
+                $this->load->view('inc/menulateral_vendedor');
+                $this->load->view('inc/menusuperior');
+                $this->load->view('marca/vendedor/marca_lista_deshabilitados',$data);
+                $this->load->view('inc/creditos');	
+                $this->load->view('inc/pie');
+            }
+        }
+	}
+
+    public function habilitarbd() 
+    {
+        $idmarca=$_POST['idmarca'];
+        $data['estado']='1';
+
+        $this->marca_model->modificarmarca($idmarca,$data);
+        redirect('marca/deshabilitados','refresh');
+    }
 
 }
--- application/models/marca_model.php
+++ application/models/marca_model.php
@@ -15,4 +15,34 @@ class Marca_model extends CI_Model {
         $this->db->insert('marca',$data);
     }
 
+    public function eliminarmarca($idmarca)
+    {
+        $this->db->where('idMarca',$idmarca);
+        $this->db->delete('marca');
+    }
+
+    public function recuperarmarca($idmarca)
+    {
+
+        $this->db->select('*');
+        $this->db->from('marca');
+        $this->db->where('idMarca',$idmarca);
+        return $this->db->get();
+    }
+
+    public function modificarmarca($idmarca,$data)
+    {
+        $this->db->where('idMarca',$idmarca);
+        $this->db->update('marca',$data);
+    }
+
+    public function listamarcasdeshabilitados()
+    {
+        $this->db->select('*');
+        $this->db->from('marca');
+        $this->db->where('estado','0');
+        return $this->db->get();
+    }
+
+
 }
