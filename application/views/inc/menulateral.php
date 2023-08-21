aa265aae4d38a574d8066544d154b58d863910d1
lore <larrea.lorena.939@gmail.com>
2023-08-20 22:08:28 -0400

    Creacion de las vistas modificar y lista deshabilitados de la marca

--- application/views/inc/menulateral.php
+++ application/views/inc/menulateral.php
@@ -60,12 +60,12 @@
                       <li>
                           <?php echo form_open_multipart('categoria/index');?>
                             <button type="submit" class="col-md-11 btn btn-dark" style="background-color: transparent; border: none;">
-                              Categoria
+                              Categorias
                             </button>
                           <?php echo form_close();?>
                       </li>
                       <li>
-                          <?php echo form_open_multipart('producto/index');?>
+                          <?php echo form_open_multipart('marca/index');?>
                             <button type="submit" class="col-md-11 btn btn-dark" style="background-color: transparent; border: none;">
                               Marcas
                             </button>
